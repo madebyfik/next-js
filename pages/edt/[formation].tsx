7bbfe8893a2f5a1e0e74d254d5eddddfc0348561
fik <35746722+madebyfik@users.noreply.github.com>
2023-02-27 14:59:34 +0100

    Update [formation].tsx

--- pages/edt/[formation].tsx
+++ pages/edt/[formation].tsx
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 import { useRouter } from "next/router";
 import {useEffect, useRef, useState} from "react";
 import FullCalendar from "@fullcalendar/react";
